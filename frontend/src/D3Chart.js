b5d23f01955e834cc1a100fcf4741bafcdd31732
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-11 15:41:18 -0700

    created the sizing dimensions for the D3 chart

--- frontend/src/D3Chart.js
+++ frontend/src/D3Chart.js
@@ -1,6 +1,18 @@
+import * as d3 from 'd3';
+
+const MARGIN = {TOP: 10, BOTTOM: 10, LEFT: 10, RIGHT: 10};
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
+
 class D3Chart {
   constructor(element) {
-    this.element = element;
+    const vis = this;
+    const g = d3.select(element)
+      .append('svg')
+        .attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+        .height('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+      .append('g')
+        .attr('transform', `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
   }
 
   update() {
