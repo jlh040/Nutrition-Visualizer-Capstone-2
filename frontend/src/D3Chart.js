9e5fa8fac5f4c3fe6b024f1ffd76b27e5b3e1d3d
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-12 13:16:51 -0700

    filled out the config file and added it to git ignore

--- frontend/src/D3Chart.js
+++ frontend/src/D3Chart.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+import { apiKey, getUrl } from './config';
 
 const MARGIN = {TOP: 10, BOTTOM: 10, LEFT: 10, RIGHT: 10};
 const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
@@ -7,15 +8,26 @@ const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
 class D3Chart {
   constructor(element) {
     const vis = this;
-    const g = d3.select(element)
+    vis.g = d3.select(element)
       .append('svg')
         .attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-        .height('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+        .attr('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
       .append('g')
         .attr('transform', `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
+
+
+
+    const rects = vis.g.selectAll('rect')
+      .data(data)
+    
+    rects.enter()
+      
+    
   }
 
   update() {
+    const vis = this;
+
 
   }
 }
