de16daa24da803069d233377ed085d586a869604
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-12 17:34:13 -0700

    removed unused dependencies

--- frontend/src/RecipeList.js
+++ frontend/src/RecipeList.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
 import VirtualizedList from './VirtualizedList';
 
 const RecipeList = () => {
@@ -33,7 +31,7 @@ const RecipeList = () => {
         style={style}
         disablePadding
       >
-        <ListItemButton role={undefined} onClick={handleToggle(index)} dense>
+        <ListItemButton onClick={handleToggle(index)} dense>
           <ListItemIcon>
             <Checkbox
               edge="start"
