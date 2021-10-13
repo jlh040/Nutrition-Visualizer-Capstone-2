c0e5b5920258cdf38a31039551737cfb493383e5
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-12 19:16:25 -0700

    VirtualizedList now has access to a list of recipes

--- frontend/src/VirtualizedList.js
+++ frontend/src/VirtualizedList.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import getUrl from './config';
 import Box from '@mui/material/Box';
 import { FixedSizeList } from 'react-window';
 
 const VirtualizedList = ({ renderRow }) => {
+  const [recipes, setRecipes] = useState(null)
+  useEffect(() => {
+    const getData = async () => {
+      const resp = await axios.get(getUrl('three'));
+      const recipes = resp.data.results.map(d => ({
+        title: d.title,
+        fat: d.nutrition.nutrients[0].amount
+      }));
+      setRecipes(recipes);
+      console.log(recipes);
+    }
+    getData();
+  }, []);
+
   return (
     <Box
       sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
