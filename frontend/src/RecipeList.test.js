c1f7ac8429401a48796248c56d8173ed9a29fe90
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-18 00:12:41 -0700

    modified a test

--- frontend/src/RecipeList.test.js
+++ frontend/src/RecipeList.test.js
@@ -8,6 +8,9 @@ it('renders without crashing', () => {
 
 // the button text tells the user to pick recipes when the page first loads
 it('does not allow the user to compare recipes upon page render', () => {
-  const { getByText } = render(<RecipeList />);
+  const { getByText, queryByText } = render(<RecipeList />);
+
   expect(getByText('Please select three recipes')).toBeInTheDocument();
-})
+  expect(queryByText('Compare')).not.toBeInTheDocument();
+});
+
