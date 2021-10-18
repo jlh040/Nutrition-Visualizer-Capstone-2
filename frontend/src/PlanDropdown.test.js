70bdde4d996778e0ecc88a9515c922361c51145d
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-17 23:37:48 -0700

    wrote a test to check that the label is showing up

--- frontend/src/PlanDropdown.test.js
+++ frontend/src/PlanDropdown.test.js
@@ -1,7 +1,13 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import PlanDropdown from './PlanDropdown';
 
 // smoke test
 it('renders without crashing', () => {
   render(<PlanDropdown />);
-})
+});
+
+// correctly renders the plan label
+it('shows the label for the dropdown', () => {
+  const { getByLabelText } = render(<PlanDropdown />);
+  expect(getByLabelText('Plan')).toBeInTheDocument();
+});
