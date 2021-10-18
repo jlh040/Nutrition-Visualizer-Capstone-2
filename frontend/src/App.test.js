81ca760fd3c37405e7ba88ca9f7fa8b389f4dbb1
Jeff Hall <jeff.leon.hall@gmail.com>
2021-10-17 21:49:13 -0700

    wrote tests that check if the getUrl function returns the right URL

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 import App from './App';
 
-test('renders without crashing', () => {
+// smoke test
+it('renders without crashing', () => {
   render(<App />);
 });
---
+++ frontend/src/config.test.js
@@ -0,0 +1,11 @@
+import getUrl from './config';
+
+it('returns the right URL when trying to bulk up', () => {
+  expect(getUrl('bulk up')).toContain('minCalories=400');
+  expect(getUrl('bulk up')).toContain('maxFat=10');
+});
+
+it('returns the right URL when trying to slim down', () => {
+  expect(getUrl('slim down')).toContain('maxFat=5');
+  expect(getUrl('slim down')).toContain('minCalories=0');
+})
